89d0b67a30e626ba8d12bf1818d364afc7a6cad1
Robodlan <reinaldo.robles@icloud.com>
2024-01-31 17:13:21 -0400

    commit
    palindrome first commit

---
+++ srcript.js
@@ -0,0 +1,38 @@
+const textInput = document.getElementById("text-input");
+const button = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
+
+
+function palindrome() {
+  let str = textInput.value; 
+  let regex = /[^a-z0-9]/gi;
+  let splitStr = str.split('').reverse().join('');
+  let otherStr = str.toLowerCase().replace(regex, '');
+  let theStr = otherStr.split('').reverse().join('');
+
+  return theStr === otherStr || splitStr === str ? 
+  result.textContent = `${textInput.value} is a palindrome` 
+  : result.textContent = `${textInput.value} is not a palindrome` ;
+};
+
+
+button.addEventListener("click", click);
+
+function click() {
+  if (!textInput.value) {
+    alert("Please input a value");
+  } else {
+    palindrome()
+  } 
+};
+
+
+
+
+
+
+
+
+
+
